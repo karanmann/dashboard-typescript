7f6ffb213a219e96693b680dad65926bf87279b6
Karan Mann <karan.mann@outlook.com>
2023-04-11 10:23:19 +0200

    dev: Initial Setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1>React App</h1>
+    </div>
+  )
+}
+
+export default App
