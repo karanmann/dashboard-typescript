ec5bd67a79da5587591adc58da95839b5995e590
Karan Mann <karan.mann@outlook.com>
2023-04-15 12:13:36 +0200

    dev: Routing setup and Navbar with reactive button

--- client/src/scenes/navbar/index.tsx
+++ client/src/scenes/navbar/index.tsx
@@ -2,14 +2,50 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import { Box, Typography, useTheme } from "@mui/material";
 import FlexBetween from "@/components/FlexBetween";
+import DashboardIcon from "@mui/icons-material/Dashboard";
 
 type Props = {};
 
 const Navbar = (props: Props) => {
   const { palette } = useTheme();
+  const [selected, setSelected] = useState("dashboard");
   return (
-    <FlexBetween mb="0.25rem" p="0.5rem 0rem">
-      hello
+    <FlexBetween mb="0.25rem" p="0.5rem 0rem" color={palette.grey[300]}>
+      {/* {LEFT SIDE} */}
+      <FlexBetween gap="0.75rem">
+        <DashboardIcon sx={{ fontSize: "28px" }} />
+        <Typography variant="h4" fontSize="16px">
+          Dashboard
+        </Typography>
+      </FlexBetween>
+
+      {/* {RIGHT SIDE} */}
+      <FlexBetween gap="2rem">
+        <Box sx={{ "&:hover": { color: palette.primary[100] } }}>
+          <Link 
+            to="/"
+            onClick={() => setSelected("dashboard")}
+            style={{
+              color: selected === "dashboard" ? "inherit" : palette.grey[700],
+              textDecoration: "inherit"
+            }}
+            >
+            DASHBOARD
+          </Link>
+        </Box>
+        <Box sx={{ "&:hover": { color: palette.primary[100] } }}>
+          <Link 
+            to="/predictions" 
+            onClick={() => setSelected("predictions")}
+            style={{
+              color: selected === "predictions" ? "inherit" : palette.grey[700],
+              textDecoration: "inherit"
+            }}
+            >
+            PREDICTIONS
+          </Link>
+        </Box>
+      </FlexBetween>
     </FlexBetween>
   );
 };
