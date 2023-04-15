33a0d4165a10e61d6b8db1b3fb7a38758ee2fdb1
Karan Mann <karan.mann@outlook.com>
2023-04-15 11:41:45 +0200

    dev: Setup routes and folder structure - use inbuilt styled comonents removed sass

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,29 @@
-import { createTheme } from "@mui/material/styles"
-import { CssBaseline, ThemeProvider } from "@mui/material"
-import { useMemo } from "react"
-import { themeSettings } from "./theme/theme"
-
+import { createTheme } from "@mui/material/styles";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { Box } from "@mui/system";
+import { useMemo } from "react";
+import { themeSettings } from "./theme/theme";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from '@/scenes/navbar'
 
 const App = () => {
-  const theme = useMemo(() => createTheme(themeSettings), [])
+  const theme = useMemo(() => createTheme(themeSettings), []);
   return (
     <div className="app">
-      <ThemeProvider theme={theme}> 
-        <CssBaseline />
-      </ThemeProvider>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem"> 
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<div>dashboard page</div>} />
+              <Route path="/predictions" element={<div>predictions</div>} />
+            </Routes>
+          </Box>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
