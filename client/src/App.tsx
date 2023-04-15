ab66c29530a36654a2a802b86bfd8b6012372548
Karan Mann <karan.mann@outlook.com>
2023-04-15 13:06:18 +0200

    dev: setup grid - grid area for the dashboard layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,8 @@ import { useMemo } from "react";
 import { themeSettings } from "./theme/theme";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from '@/scenes/navbar'
+import Dashboard from "./scenes/dashboard";
+import Predictions from "./scenes/predictions";
 
 const App = () => {
   const theme = useMemo(() => createTheme(themeSettings), []);
@@ -16,8 +18,8 @@ const App = () => {
           <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem"> 
             <Navbar />
             <Routes>
-              <Route path="/" element={<div>dashboard page</div>} />
-              <Route path="/predictions" element={<div>predictions</div>} />
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/predictions" element={<Predictions />} />
             </Routes>
           </Box>
         </ThemeProvider>
