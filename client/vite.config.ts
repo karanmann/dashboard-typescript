35257ead7ddf756819805e12ae8170dfe3afc152
Karan Mann <karan.mann@outlook.com>
2023-04-15 10:48:23 +0200

    dev: setup relative path insted of absolute

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import sass from 'sass'
-
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [{
+      find: '@',
+      replacement: path.resolve(__dirname, 'src')
+    }]
+  },
   css: {
     preprocessorOptions: {
       scss: {
