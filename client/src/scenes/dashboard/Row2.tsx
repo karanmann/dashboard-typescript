41cda4910288da593e4a4303f9d53c79d58c7a20
Karan Mann <karan.mann@outlook.com>
2023-04-30 21:29:40 +0200

    dev: Pie chart Typography content added

--- client/src/scenes/dashboard/Row2.tsx
+++ client/src/scenes/dashboard/Row2.tsx
@@ -1,8 +1,9 @@
 import React, { useMemo } from "react";
-import { useTheme } from "@mui/material";
+import { Box, Typography, useTheme } from "@mui/material";
 import BoxHeader from "@/components/BoxHeader";
 import DashboardBox from "@/components/DashboardBox";
 import { useGetKpisQuery, useGetProductsQuery } from "@/state/api";
+import FlexBetween from "@/components/FlexBetween";
 import {
   ResponsiveContainer,
   CartesianGrid,
@@ -27,7 +28,7 @@ const Row2 = (props: Props) => {
 
   const pieData = [
     { name: "Group A", value: 600 },
-    { name: "Group B", value: 400 }
+    { name: "Group B", value: 400 },
   ];
   const pieColors = [palette.primary[800], palette.primary[300]];
 
@@ -100,31 +101,43 @@ const Row2 = (props: Props) => {
         </ResponsiveContainer>
       </DashboardBox>
       <DashboardBox gridArea="e">
-        <PieChart 
-          width={110} 
-          height={100}
-          margin={{
-            top: 0,
-            right: -10,
-            left: 10,
-            bottom: 0,
-          }}
+        <BoxHeader title="Campaigns and Targets" sideText="+9%" />
+        <FlexBetween mt="0.25rem" gap="1.5rem" pr="1rem">
+          <PieChart
+            width={110}
+            height={100}
+            margin={{
+              top: 0,
+              right: -10,
+              left: 10,
+              bottom: 0,
+            }}
           >
-          <Pie
-            data={pieData}
-            innerRadius={18}
-            outerRadius={38}
-            paddingAngle={2}
-            dataKey="value"
-          >
-            {pieData.map((entry, index) => (
-              <Cell
-                key={`cell-${index}`}
-                fill={pieColors[index]}
-              />
-            ))}
-          </Pie>
-        </PieChart>
+            <Pie
+              data={pieData}
+              innerRadius={18}
+              outerRadius={38}
+              paddingAngle={2}
+              dataKey="value"
+              stroke="none"
+            >
+              {pieData.map((entry, index) => (
+                <Cell key={`cell-${index}`} fill={pieColors[index]} />
+              ))}
+            </Pie>
+          </PieChart>
+          <Box ml="-0.7rem" flexBasis="40%" textAlign="center">
+            <Typography variant="h5">Target Sales</Typography>
+            <Typography m="0.3rem 0" color={palette.primary[300]}>83</Typography>
+            <Typography variant="h6">Finance goals of the campaign that is desired</Typography>
+          </Box>
+          <Box flexBasis="40%">
+            <Typography variant="h5">Losses in Revenue</Typography>
+            <Typography variant="h6">Losses are down 25%</Typography>
+            <Typography mt="0.4rem" variant="h5">Profit Margins</Typography>
+            <Typography variant="h6">Margin are up by 30% from last month</Typography>
+          </Box>
+        </FlexBetween>
       </DashboardBox>
       <DashboardBox gridArea="f"></DashboardBox>
     </>
