37b08efef1e4c507530bec7f718188ee8d60592a
Karan Mann <karan.mann@outlook.com>
2023-05-01 13:01:51 +0200

    dev: Basic Prediction page layout

--- client/src/scenes/predictions/index.tsx
+++ client/src/scenes/predictions/index.tsx
@@ -1,12 +1,27 @@
-import { Box, useTheme } from "@mui/material"
+import { useState } from "react";
+import { Box, Typography, useTheme } from "@mui/material";
+import { useGetKpisQuery } from "@/state/api";
+import DashboardBox from "@/components/DashboardBox";
+import FlexBetween from "@/components/FlexBetween";
 
-type Props = {}
+type Props = {};
 
 const Predictions = (props: Props) => {
-  const { palette } = useTheme()
-  return (
-    <Box color={palette.grey[300]}>Predictions</Box>
-  )
-}
+  const { palette } = useTheme();
+  const [isPredictions, setIsPredictions] = useState(false);
 
-export default Predictions
+  const { data: kpiData } = useGetKpisQuery();
+
+  return (
+    <DashboardBox width="100%" height="100%" p="1rem" overflow="hidden">
+      <FlexBetween m="1rem 2.5rem">
+        <Box>
+          <Typography variant="h3">Revenue and Predictions</Typography>
+          <Typography variant="h6">Charted revenue and predicted revenue based on a simple linear regression model</Typography>
+        </Box>
+      </FlexBetween>
+    </DashboardBox>
+  );
+};
+
+export default Predictions;
