599923c9e77f80d4e3e6a7e271115ebb1860683f
Karan Mann <karan.mann@outlook.com>
2023-05-02 10:47:43 +0200

    Delete ReadMe.md

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -15,7 +15,7 @@ const App = () => {
       <BrowserRouter>
         <ThemeProvider theme={theme}>
           <CssBaseline />
-          <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem"> 
+          <Box width="100%" height="90%" padding="1rem 2rem 4rem 2rem">
             <Navbar />
             <Routes>
               <Route path="/" element={<Dashboard />} />
